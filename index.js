55de4ebbb1a8c074b9256b69aae77174bed3e072
Emerson Alan <ee2018allan@gmail.com>
2025-08-17 20:26:12 -0300

    commit erro

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function adicionarAoCarrinho(produto) {
-    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    let carrinho = JSON.parse(localStorage.getItem('carinho')) || [];
     carrinho.push(produto);
     localStorage.setItem('carrinho', JSON.stringify(carrinho));
     alert(`${produto.nome} foi adicionado ao carrinho!`);
