81e9b3715f99d520ab58fdfd48a613e62d8a0b45
Emerson Alan <ee2018allan@gmail.com>
2025-08-17 20:47:31 -0300

    Bugs corrigidos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
+
 function adicionarAoCarrinho(produto) {
-    let carrinho = JSON.parse(localStorage.getItem('carinho')) || [];
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
     carrinho.push(produto);
     localStorage.setItem('carrinho', JSON.stringify(carrinho));
     alert(`${produto.nome} foi adicionado ao carrinho!`);
